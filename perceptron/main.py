94b19e568dc78268c9bf4ecb39138b18e1a2f221
Ilya Ivanov <ilyaivanov654@gmail.com>
2022-08-14 20:08:24 +0300

    saving the model, checkpoints

--- perceptron/main.py
+++ perceptron/main.py
@@ -1,24 +1,16 @@
-import matplotlib.pyplot as plt
+import datetime
+import os
+
 import tensorflow as tf
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Flatten
 from tensorflow.python.keras.layers import Dense
 
-
-def plot_loss(history):
-    plt.plot(history.history['loss'], label='train loss')
-    plt.plot(history.history['val_loss'], label='test loss')
-    plt.ylim([0, 2])
-    plt.xlabel('Epoch')
-    plt.ylabel('Error [MPG]')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+# todo refactoring
+# todo readme
 
 
 if __name__ == '__main__':
-    # check out the TensorFlow version
-    print(tf.__version__)
     # load the MNIST data
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
@@ -44,12 +36,30 @@ if __name__ == '__main__':
         Dense(hidden_layer_2_neurons_count, activation='relu'),
         Dense(outputs_count, activation='softmax')
     ])
+
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    history = model.fit(x_train, y_train, epochs=epochs_count, batch_size=batch_size, validation_split=0.2)
+
+    # creating a log directory to store data for visualization
+    log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    # execute 'tensorboard --logdir logs/' to run TensorBoard with analysis result
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    checkpoint_path = "checkpoints/training_1/cp.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    # create a callback that saves the model's weights
+    # model.load_weights(checkpoint_path) to load weights from the checkpoint
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                     save_weights_only=True,
+                                                     verbose=1)
+
+    history = model.fit(x_train, y_train, epochs=epochs_count, batch_size=batch_size, validation_split=0.2,
+                        callbacks=[tensorboard_callback, cp_callback])
+
     results = model.evaluate(x_test, y_test, verbose=0)
     print('Test loss, test accuracy:', results)  # ~97% accuracy
 
     # 2
     print("Prediction:", model.predict(x_test[1:2]))
 
-    plot_loss(history)
+    # save the entire model as a SavedModel
+    model.save('saved_model/my_model')
