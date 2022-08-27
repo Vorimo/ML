eac237433da35b7499e7ce2d16fe3437b9d9dc92
Ilya Ivanov <ilyaivanov654@gmail.com>
2022-08-27 16:28:34 +0300

    README added

---
+++ cnn/README.md
@@ -0,0 +1,2 @@
+This project shows a basic CNN build to classify CIFAR10 data. Project contains data loading and visualization,
+model build and summary, model execution and evaluation console/graphic results 
--- cnn/main.py
+++ cnn/main.py
@@ -2,7 +2,6 @@ from cnn.datasets import get_prepared_cifar_10_data
 from cnn.model import fit_and_evaluate_model, build_model
 from cnn.plotting import show_cifar_10_preview, show_model_evaluation
 
-# todo readme
 if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = get_prepared_cifar_10_data()
 
