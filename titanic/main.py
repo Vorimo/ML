1a97284cdf998ec64a1592ac38432893633889b7
Vorimo <ilyaivanov654@gmail.com>
2022-11-15 21:31:35 +0300

    refactoring

--- titanic/main.py
+++ titanic/main.py
@@ -39,7 +39,7 @@ def plot_model_loss(history):
 if __name__ == '__main__':
     x_train_df = pd.read_csv('datasets/train.csv')
     y_train_df = x_train_df['Survived']
-    x_train_df.drop(['Survived', 'PassengerId', 'Name', 'Ticket', 'Fare', 'Embarked', 'Cabin'], axis=1, inplace=True)
+    x_train_df.drop(['Survived', 'PassengerId', 'Name', 'Ticket', 'Fare', 'Cabin', 'Embarked'], axis=1, inplace=True)
     x_train_df['Age'].fillna(x_train_df['Age'].mean(), inplace=True)
     x_train_df['Sex'].replace({'female': 0, 'male': 1}, inplace=True)
     x_train = x_train_df.to_numpy()
@@ -48,7 +48,7 @@ if __name__ == '__main__':
 
     model = Sequential()
     model.add(Dense(5, input_shape=(5,), activation='relu'))
-    model.add(Dense(10, activation='relu'))
+    model.add(Dense(5, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
 
     model.compile(loss='binary_crossentropy',
@@ -60,3 +60,18 @@ if __name__ == '__main__':
     history = model.fit(x_train[100:], y_train[100:], epochs=epochs, batch_size=batches,
                         validation_data=(x_train[:100], y_train[:100]))
     show_model_evaluation(history)
+
+    x_test_df = pd.read_csv('datasets/test.csv')
+    passenger_ids = x_test_df['PassengerId']
+    x_test_df.drop(['PassengerId', 'Name', 'Ticket', 'Fare', 'Cabin', 'Embarked'], axis=1, inplace=True)
+    x_test_df['Age'].fillna(x_train_df['Age'].mean(), inplace=True)
+    x_test_df['Sex'].replace({'female': 0, 'male': 1}, inplace=True)
+    x_test = MinMaxScaler().fit_transform(x_test_df)
+    result = pd.DataFrame({'PassengerId': passenger_ids})
+    predictions = []
+    for test_input in x_test:
+        prediction = model.predict(test_input.reshape(1, -1))[0][0]
+        predictions.append(round(prediction))
+    result['Survived'] = predictions
+    result = result.astype({"PassengerId": "int", "Survived": "int"})
+    result.to_csv('datasets/prediction.csv', chunksize=100, index=False)
