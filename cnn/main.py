4090f2d2d57c66bed9846bdfad45353b7139dd46
Vorimo <ilyaivanov654@gmail.com>
2022-12-01 11:34:53 +0300

    cifar dataset classification model upgraded to 82%

--- cnn/main.py
+++ cnn/main.py
@@ -5,9 +5,6 @@ from cnn.plotting import show_model_evaluation
 if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = get_prepared_cifar_10_data()
 
-    x_val, y_val = x_train[:10000], y_train[:10000]
-    partial_x_train, partial_y_train = x_train[10000:], y_train[10000:]
-
-    history = build_model(partial_x_train, partial_y_train, x_val, y_val, x_test, y_test)
+    history = build_model(x_train, y_train, x_test, y_test)
 
     show_model_evaluation(history)
--- cnn/model.py
+++ cnn/model.py
@@ -1,32 +1,50 @@
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, BatchNormalization, Dropout
 from keras.models import Sequential
 
 
-def build_model(x_train, y_train, x_val, y_val, x_test, y_test):
+def build_model(x_train, y_train, x_val, y_val):
     # build base convolution and pooling layers
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same',
+                     input_shape=(x_train.shape[1], x_train.shape[2], x_train.shape[3])))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', ))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same',
+                     input_shape=(x_train.shape[1], x_train.shape[2], x_train.shape[3])))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same',
+                     input_shape=(x_train.shape[1], x_train.shape[2], x_train.shape[3])))
+    model.add(BatchNormalization())
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.5))
+
     # add full connection layers
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
 
     print(model.summary())
-    # todo bad result! ~72%
 
-    model.compile(optimizer='rmsprop',
+    model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
     epochs = 15
-    batches = 100
+    batches = 64
     history = model.fit(x_train, y_train, epochs=epochs, batch_size=batches,
                         validation_data=(x_val, y_val))
-
-    print('Control evaluation data:')
-    model.evaluate(x_test, y_test)
+    # result ~82%
     return history
